ecad7c994200608e72779584a78471b75685b4cd
moi <wxopwx@gmail.com>
2018-03-15 15:50:13 +0100

    rm cdn assets

--- php/page.php
+++ php/page.php
@@ -127,11 +127,6 @@ EOF;
 	<!-- ####### FOOTER ################################################################################################## -->
 	</div><!-- end main container -->
 
-    <!--
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
-    -->
     <script src="{$static}/js/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
     <script src="{$static}/js/popper.min.js" crossorigin="anonymous"></script>
     <script src="{$static}/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
