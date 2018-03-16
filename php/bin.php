ddb48e0b13a406b4e71de0dcb5c087da6c1e01ad
moi <wxopwx@gmail.com>
2018-03-16 08:54:35 +0100

    daemon and bin

--- php/bin.php
+++ php/bin.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-require_once(dirname(__FILE__).'/command.php');
-$command=new NestorCommand();
+require_once(dirname(__FILE__).'/controllers/command.php');
+$command=new NestorController_command();
 $command->run();
 ?>
---
+++ php/bin_daemon_clock.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+require_once(dirname(__FILE__).'/controllers/daemon.php');
+$command=new NestorController_daemon();
+$command->run_clock();
+?>
